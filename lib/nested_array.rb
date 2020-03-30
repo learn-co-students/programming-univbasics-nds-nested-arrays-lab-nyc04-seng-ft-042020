e3060d78f9e1a216d0422fd642ed34e3bd44027c
Fred C <canofred@gmail.com>
2020-03-30 02:16:14 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -32,9 +32,9 @@ end
   # However, this time, sort each internal array alphabetically by the first character
 
 def sorted_matrix
-new_array = []
-new_array.push(CONVENTIONAL_PRODUCE.sort!, ORGANIC_PRODUCE.sort!)
-new_array
+  new_array = []
+  new_array.push(CONVENTIONAL_PRODUCE.sort, ORGANIC_PRODUCE.sort)
+  new_array
 end
 
 
@@ -48,8 +48,11 @@ end
 
 
 
-def matrix_update(matrix, row, column, new_value)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
+
+def matrix_update(matrix, row, column, new_value)
+  matrix[row][column] = new_value
+  matrix
 end
